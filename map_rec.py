0d59d8c77eeabcb61b9648906f6dde331881f1b7
vic-dti <victor.yano@dtidigital.com.br>
2025-06-08 22:10:39 -0300

    add: render requirements

--- map_rec.py
+++ map_rec.py
@@ -815,4 +815,4 @@ def update_table_and_selection(map_bounds, initial_bounds, geojson,
 # EXECUTA O SERVIDOR
 # ───────────────────────────────────────────────────────────
 if __name__ == '__main__':
-    app.run(debug=True, port=8050)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8050)))
